0aa7488fe9ba0b7a81ed5b91f72eac0f10eac6c8
sanvean74 <dlambertpdx@gmail.com>
2019-08-13 11:27:10 -0700

    started filter sort stretch

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ import renderHeader from '../src/render-header.js';
 
 const horns = document.querySelector('.horns');
 const body = document.getElementById('body');
+const radioInput = document.getElementsByName()
 
 const headerHtml = renderHeader();
 const headerDom = htmlToDOM(headerHtml);
 body.prepend(headerDom);
-
 
 images.forEach(function(item) {
     const html = renderImage(item);
